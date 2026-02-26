7526b0a69e7bd5c9ceaa8308ae658ef12ddf0540
EVILLEGAS-9R <villegas.ej@obmontessori.edu.ph>
2026-02-26 23:29:27 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+from pyscript import display, document
+
+
+def handle_signup(e):
+    e.preventDefault()
+    output_div = document.getElementById("signup-message")
+    # Clear to prevent repeating messages
+    output_div.innerHTML = ""
+    display("Account created! You may now log in.", target="signup-message")
+
+    document.getElementById("user_name").value = ""
+
+def check_intramurals(e):
+    output_div = document.getElementById("result")
+    output_div.innerHTML = ""
+
+
+    is_registered = document.getElementById("reg_yes").checked
+    is_medical = document.getElementById("med_yes").checked
+    grade = int(document.getElementById("grade").value)
+    section = document.getElementById("section").value
+
+    if not is_registered:
+        display("Please complete the online registration.", target="result")
+    elif not is_medical:
+        display("Please secure a medical clearance.", target="result")
+    elif grade < 7 or grade > 10:
+        display("You are not eligible for Intramurals.", target="result")
+    else:
+    
+        team = ""
+        if grade == 7:
+            team = "Blue Bears"
+        elif grade == 8:
+            team = "Red Bulldogs"
+        elif grade == 9:
+            team = "Yellow Tigers"
+        elif grade == 10:
+           
+            if section == "Ruby":
+                team = "Blue Bears"
+            elif section == "Topaz":
+                team = "Red Bulldogs"
+            elif section == "Sapphire":
+                team = "Green Hornets"
+            elif section == "Emerald":
+                team = "Yellow Tigers"
+        
+        display("Congratulations! You are part of the " + team, target="result")
+
+
+def display_players():
+  
+    classmates = [
+        "Juan Dela Cruz", "Dylan Harper", "Victor Wenbanyama", 
+        "Brook Lopez", "Mark Villanueva", "Dwight Ramos", 
+        "Alpen Sengun", "Elena Smith", "Miguel Tan", "Lebron James"
+    ]
+    
+    player_list_element = document.getElementById("playerList")
+    
+    if player_list_element:
+        player_list_element.innerHTML = "" # Clear previous list
+        
+        
+        for student in classmates:
+            li = document.createElement("li")
+            li.className = "list-group-item" 
+            li.innerText = student
+            player_list_element.appendChild(li)
+
+
+if document.getElementById("playerList"):
+    display_players()
